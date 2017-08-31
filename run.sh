fa043486cad25ddf5bd84f968aa08864b7102645
Carlos Braile <cafilho99@gmail.com>
2017-08-31 17:17:51 -0300

    Fixed data centralization

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@
 #######################
 
 # > Absolute or relative path to the input file
-#FILE_PATH="/path/to/file.off"
-FILE_PATH="/media/braile/HDD/Workspace/Datasets/ModelNet10/sofa/train/sofa_0001.off"
+FILE_PATH="/path/to/file.off"
 # > Absolute or relative path to the output file
-#OUTPUT_PATH="/path/to/output/file.pcd"
-OUTPUT_PATH=".output/out.pcd"
+OUTPUT_PATH="/path/to/output/file.pcd"
 # > The step size for the rastering algorithm (between 0 and 1)
 STEP_SIZE="0.1"
 # > Enable visualization. Comment this line to disable visualization.
